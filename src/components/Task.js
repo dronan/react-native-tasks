d5f4e80706c1c8c3031a3f77d581a496335a4008
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-17 14:35:40 +0100

    return view status

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default props => {
   return (
     <View style={styles.container}>
-      <View style={styles.checkContainer}></View>
+      <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
       <View>
         <Text style={styles.description}>{props.desc}</Text>
         <Text style={styles.x}>{props.estimateAt + ''}</Text>
@@ -12,6 +13,18 @@ export default props => {
     </View>
   );
 };
+
+function getCheckView(doneAt) {
+  if (doneAt !== null) {
+    return (
+      <View style={styles.done}>
+        <Icon name="check" size={15} color="#FFF" />
+      </View>
+    );
+  } else {
+    return <View style={styles.pending}></View>;
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -24,6 +37,21 @@ const styles = StyleSheet.create({
   },
   checkContainer: {
     width: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pending: {
+    width: 25,
+    height: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#555',
+  },
+  done: {
+    width: 25,
+    height: 25,
+    borderRadius: 13,
+    backgroundColor: '#4D7031',
     alignItems: 'center',
     justifyContent: 'center',
   },
