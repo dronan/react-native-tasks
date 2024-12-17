7ddb66b33267207eec1556afd1ff57183be2f427
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-17 20:02:17 +0100

    working buttons with TouchableOpacity and TextInput

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -5,9 +5,15 @@ import {
   StyleSheet,
   Modal,
   TouchableWithoutFeedback,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
 import commonStyles from '../commonStyles';
+
+const initialState = {desc: ''};
 export default class AddTask extends Component {
+  state = {...initialState};
+
   render() {
     return (
       <Modal
@@ -20,6 +26,20 @@ export default class AddTask extends Component {
         </TouchableWithoutFeedback>
         <View style={styles.container}>
           <Text style={styles.header}>New Task</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Insert the task..."
+            onChangeText={desc => this.setState({desc})}
+            value={this.state.desc}
+          />
+          <View style={styles.buttons}>
+            <TouchableOpacity onPress={this.props.onCancel}>
+              <Text style={styles.button}>Cancel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.props.onSave}>
+              <Text style={styles.button}>Save</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <TouchableWithoutFeedback onPress={this.props.onCancel}>
           <View style={styles.background}></View>
@@ -45,5 +65,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 15,
     fontSize: 18,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    margin: 20,
+    marginRight: 30,
+    color: commonStyles.colors.today,
+  },
+  input: {
+    fontFamily: commonStyles.fontFamily,
+    height: 40,
+    margin: 15,
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#E3E3E3',
+    borderRadius: 6,
   },
 });
