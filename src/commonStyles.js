f797fb37fecd50c0eb976370ca7331869c9fb01c
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-17 19:54:31 +0100

    creating new screen

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -1,6 +1,9 @@
 export default {
   fontFamily: 'Lato',
   colors: {
+    today: '#B13B44',
+    tomorrow: '#C9742E',
+    nextWeek: '#15721E',
     secondary: '#FFF',
     mainText: '#222',
     subText: '#555',
--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Modal, TouchableWithoutFeedback} from 'react-native';
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  Modal,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import commonStyles from '../commonStyles';
 export default class AddTask extends Component {
   render() {
     return (
@@ -9,6 +15,12 @@ export default class AddTask extends Component {
         visible={this.props.isVisible}
         onRequestClose={this.props.onCancel}
         animationType="slide">
+        <TouchableWithoutFeedback onPress={this.props.onCancel}>
+          <View style={styles.background}></View>
+        </TouchableWithoutFeedback>
+        <View style={styles.container}>
+          <Text style={styles.header}>New Task</Text>
+        </View>
         <TouchableWithoutFeedback onPress={this.props.onCancel}>
           <View style={styles.background}></View>
         </TouchableWithoutFeedback>
@@ -21,5 +33,17 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  header: {
+    fontFamily: commonStyles.fontFamily,
+    backgroundColor: commonStyles.colors.today,
+    color: commonStyles.colors.secondary,
+    textAlign: 'center',
+    padding: 15,
+    fontSize: 18,
   },
 });
