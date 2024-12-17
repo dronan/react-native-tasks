6974c7a2d3dff3de7e88ea04c6aeddbda37008f5
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-17 13:54:47 +0100

    styling the header and using moment

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -3,13 +3,20 @@ import {View, Text, ImageBackground, StyleSheet} from 'react-native';
 
 import todayImage from '../../assets/imgs/today.jpg';
 
+import moment from 'moment';
+
 export default class TaskList extends Component {
   render() {
+    const today = moment().locale('en-US').format('ddd, D MMMM');
+
     return (
       <View style={styles.container}>
-        <ImageBackground
-          source={todayImage}
-          style={styles.background}></ImageBackground>
+        <ImageBackground source={todayImage} style={styles.background}>
+          <View style={styles.titleBar}>
+            <Text>Today</Text>
+            <Text>{today}</Text>
+          </View>
+        </ImageBackground>
         <Text style={styles.taskList}>Task List</Text>
       </View>
     );
@@ -25,5 +32,9 @@ const styles = StyleSheet.create({
   },
   taskList: {
     flex: 7, // 70% of the screen
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
   },
 });
