03efebcaa509ae3b7e6e0ef9572c426208dd615a
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-20 21:28:09 +0100

    imporving error return

--- src/common.js
+++ src/common.js
@@ -4,7 +4,11 @@ const server =
   Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
 
 function showError(err) {
-  Alert.alert('Error trying to connect!', `Message: ${err}`);
+  if (err.response && err.response.data) {
+    Alert.alert('Ops! An error occurred!', `${err.response.data}`);
+  } else {
+    Alert.alert('Ops! An error occurred!', `${err}`);
+  }
 }
 
 function showSuccess(msg) {
