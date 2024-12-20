ebb4998328cd0247a2eb3f42019a34aa608c5c0c
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-20 17:59:22 +0100

    add drawer navigation to filter views

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,7 +1,7 @@
-// /src/Navigator.js
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
@@ -9,6 +9,38 @@ import AuthComponent from './screens/Auth';
 import TaskListComponent from './screens/TaskList';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Today">
+      <Drawer.Screen
+        name="Today"
+        component={TaskListComponent}
+        initialParams={{daysAhead: 0}}
+        options={{headerShown: false, title: 'Today'}}
+      />
+      <Drawer.Screen
+        name="Tomorrow"
+        component={TaskListComponent}
+        initialParams={{daysAhead: 1}}
+        options={{headerShown: false, title: 'Tomorrow'}}
+      />
+      <Drawer.Screen
+        name="Week"
+        component={TaskListComponent}
+        initialParams={{daysAhead: 7}}
+        options={{headerShown: false, title: 'Week'}}
+      />
+      <Drawer.Screen
+        name="Month"
+        component={TaskListComponent}
+        initialParams={{daysAhead: 30}}
+        options={{headerShown: false, title: 'Month'}}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 const Navigator = () => {
   return (
@@ -23,7 +55,7 @@ const Navigator = () => {
             />
             <Stack.Screen
               name="Home"
-              component={TaskListComponent}
+              component={DrawerNavigator}
               options={{headerShown: false}}
             />
           </Stack.Navigator>
