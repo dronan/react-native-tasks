bdf2f00e4901b5879b978a5cc9fca7cf8fd84434
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-20 21:21:24 +0100

    implement logout feature

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import {
   DrawerContentScrollView,
   DrawerItemList,
@@ -8,10 +8,20 @@ import {useRoute} from '@react-navigation/native';
 import commonStyles from '../commonStyles';
 import Avatar from '../components/Avatar';
 
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 export default props => {
   const route = useRoute();
   const userData = route.params || {};
   const {avatarUrl, name, email} = userData;
+
+  const logout = async () => {
+    delete axios.defaults.headers.common['Authorization'];
+    await AsyncStorage.removeItem('userData');
+    props.navigation.navigate('AuthOrApp');
+  };
 
   return (
     <DrawerContentScrollView {...props}>
@@ -24,7 +34,18 @@ export default props => {
           <Text style={styles.userName}>{name}</Text>
           <Text style={styles.userEmail}>{email}</Text>
         </View>
+        <TouchableOpacity onPress={logout}>
+          <View style={styles.logout}>
+            <Icon
+              name="sign-out"
+              style={styles.logoutIcon}
+              size={20}
+              color="#800"
+            />
+          </View>
+        </TouchableOpacity>
       </View>
+
       <DrawerItemList {...props} />
     </DrawerContentScrollView>
   );
@@ -32,7 +53,7 @@ export default props => {
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'column', // Alinha os itens na horizontal
+    flexDirection: 'column',
     alignItems: 'flex-start',
     borderBottomWidth: 1,
     borderColor: '#DDD',
@@ -61,5 +82,13 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: commonStyles.colors.subText,
     paddingLeft: 10,
+  },
+  logout: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  logoutIcon: {
+    color: '#800',
   },
 });
